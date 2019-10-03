4ad8a3e775b97f12891deea6bbaf4bf5021c3dcb
89465 <89465@farnborough.ac.uk>
2019-10-03 14:31:00 +0100

    + api added

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -1,6 +1,17 @@
+package Controllers;
+
+import Server.Main;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+@Path("users/")
 public class UserController {
     // Add a new user to the database
     public static void addUser(String username, String DOB) {
@@ -33,23 +44,28 @@ public class UserController {
 
 
     // Print the entire database to console
-    public static void listUsers() {
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String listUsers() {
+        JSONArray list = new JSONArray();
         try {
 
             PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, Username, DOB FROM Usernames");
 
             ResultSet results = ps.executeQuery();
-            System.out.println("Usernames:");
             while (results.next()) {
-                int userID = results.getInt(1);
-                String username= results.getString(2);
-                String dob = results.getString(3);
-
-                System.out.println(userID + ":  " + username + " " + dob);
+                JSONObject item = new JSONObject();
+                item.put("userID", results.getInt(1));
+                item.put("username", results.getString(2));
+                item.put("DOB", results.getString(3));
+                list.add(item);
             }
+            return list.toString();
 
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to list items, please see server console for more info.\"}";
         }
     }
 
