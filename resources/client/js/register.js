918546be4954e8a8b4a677279998c59bb1ff58d8
89465 <89465@farnborough.ac.uk>
2020-01-09 14:30:13 +0000

    + registering and error message for taken usernames

--- resources/client/js/register.js
+++ resources/client/js/register.js
@@ -2,4 +2,20 @@ function pageLoad() { document.getElementById("loginButton").addEventListener("c
 
 function register(event) {
 
+    event.preventDefault();
+
+    const form = document.getElementById("loginForm");
+    const formData = new FormData(form);
+
+    fetch("/users/add", {method: 'post', body: formData}
+    ).then(response => response.json()
+    ).then(responseData => {
+
+        if (responseData.hasOwnProperty('error')) {
+            document.getElementById("error").innerHTML = "Username already taken.";
+        } else {
+            alert("Account successfully created.");
+            window.location.href = '/client/login.html';
+        }
+    });
 }
